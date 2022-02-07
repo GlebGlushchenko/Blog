b116393df8cee8feb1f80de1262c186334a9fbc8
Ваше Имя <you@example.com>
2022-02-07 21:59:26 +0300

    add sidebar

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+// const root = document.querySelector(':root')
+// const rootStyle = getComputedStyle(root)
+// const primeColor = rootStyle.getPropertyValue('--prime-bg--color')
+// console.log(primeColor)
+
+// root.style.setProperty('--prime-bg--color', 'black')
